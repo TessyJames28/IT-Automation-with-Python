70147824e4b975956295c1b4a76fa3465bcb411a
Tessyjames <tessyjames28@gmail.com>
2024-04-20 13:05:02 +0100

    Regex

---
+++ Interacting_with_OS/Regular_Expressions/regex.py
@@ -0,0 +1,26 @@
+import re
+
+print(re.search(r"A.*a", "Argentina"))
+print(re.search(r"A.*a", "Azerbaijan"))
+print(re.search(r"^A.*a$", "Australia"))
+
+pattern = r"^[a-zA-Z_][a-zA-Z0-9_]*$"
+print(re.search(pattern, "_this_is_a_valid_variable_name"))
+print(re.search(pattern, "this isn't a valid variable"))
+print(re.search(pattern, "my_variable1"))
+print(re.search(pattern, "2my_variable1"))
+
+
+# Fill in the code to check if the text passed looks like a standard sentence, meaning that it starts with an uppercase letter, followed by at least some lowercase letters or a space, and ends with a period, question mark, or exclamation point. 
+def check_sentence(text):
+  result = re.search(r"^[A-Z][a-z\s]*[.?!]$", text)
+  return result != None
+
+print(check_sentence("Is this is a sentence?")) # True
+print(check_sentence("is this is a sentence?")) # False
+print(check_sentence("Hello")) # False
+print(check_sentence("1-2-3-GO!")) # False
+print(check_sentence("A star is born.")) # True
+print(check_sentence("A star is born")) # False
+print(check_sentence("A Star is born")) # False
+
