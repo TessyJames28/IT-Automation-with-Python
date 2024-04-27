73a9ce79d1461f1b3e5a39309b58e88f614c91e7
Tessyjames <tessyjames28@gmail.com>
2024-04-27 01:34:43 +0100

    Grouping

---
+++ Interacting_with_OS/Regular_Expressions/capturing_groups.py
@@ -0,0 +1,20 @@
+import re
+
+result = re.search(r"^(\w*), (\w*)$", "Lovelace, Ada")
+print(result)
+print(result.groups())
+print(result[0])
+print(result[1])
+print(result[2])
+print("{} {}".format(result[2], result[1]))
+
+def rearrange_name(name):
+    result = re.search(r"^(\w*), (\w*)$", name)
+    if result is None:
+        return name
+    return "{} {}".format(result[2], result[1])
+
+
+print(rearrange_name("Lovelace, Ada"))
+print(rearrange_name("Ritchie, Dennis"))
+print(rearrange_name("Hopper, Grace M."))
